97f03fdda2196096ce508165de88c6d8194281be
chargeAhead <ndugirecharles@gmail.com>
2021-04-13 01:12:14 +0300

    Add migrations dependencies

--- project/migrations/0003_auto_20201102_2010.py
+++ project/migrations/0003_auto_20201102_2010.py
@@ -6,14 +6,6 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('project', '0002_auto_20201102_0059'),
-    ]
-
-    operations = [
-        migrations.AlterField(
-            model_name='neighbourhood',
-            name='neighbourhood_logo',
-            field=cloudinary.models.CloudinaryField(max_length=255, verbose_name='image'),
+    cloudinary.models.CloudinaryField(max_length=255, verbose_name='image'),
         ),
     ]
